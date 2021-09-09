36eb07f137f2fb8a0d6214d0d932117bec7a8a20
th4tkh13m <khiemtonthat@gmail.com>
2021-09-09 13:57:48 +0700

    Add students

---
+++ Day1/studentdemo/Main.java
@@ -0,0 +1,11 @@
+package studentdemo;
+
+public class Main {
+    public static void main(String[] args) {
+        Student stu01 = new Student("Paul Lee", 3.5);
+        Student stu02 = new Student("Oh sad", 4.0);
+        System.out.println(""+stu01.toString());
+        System.out.println((""+stu02.toString()));
+        stu01.checkActivity(5);
+    }
+}
---
+++ Day1/studentdemo/Student.java
@@ -0,0 +1,33 @@
+package studentdemo;
+
+public class Student {
+    private String name;
+    private double gpa;
+
+    public Student(String name, double gpa) {
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public void goToUni() {
+        System.out.println("Go to Uni every morning");
+    }
+
+    public void hobbies() {
+        System.out.println("Vovinam");
+    }
+
+    public void takingFinalExam() {
+        System.out.println("Taking the online exam. Bruh");
+    }
+
+    public void checkActivity(int time) {
+        if (time == 5) {
+            this.takingFinalExam();
+        }
+    }
+    
+    public String toString() {
+        return "Student{" + "name=" + name + ", gpa=" + gpa + '}';
+    }
+}
