ea8f41459e864f199a616cb54606ad13f6d6d6ee
th4tkh13m <khiemtonthat@gmail.com>
2021-09-16 13:12:48 +0700

    Add day2

---
+++ Day2/src/StudentDemo/Main.java
@@ -0,0 +1,11 @@
+package StudentDemo;
+
+public class Main {
+   
+    public static void main(String[] args){
+        Student stu01 = new Student();
+        System.out.println("" + stu01);
+        stu01.input();
+        stu01.output();
+    }
+}
---
+++ Day2/src/StudentDemo/Student.java
@@ -0,0 +1,36 @@
+package StudentDemo;
+
+import java.util.Scanner;
+
+public class Student {
+   
+    Scanner in = new Scanner(System.in);
+
+    private String code;
+    private String name;
+    private int bYear;
+    private String address;
+
+    public Student() {
+
+    }
+
+    public void input() {
+        System.out.println("Enter code: ");
+        code = in.nextLine();
+        System.out.println("Enter name: ");
+        name = in.nextLine();
+        System.out.println("Enter year: ");
+        bYear = in.nextInt();
+        in.nextLine();
+        System.out.println("Enter address: ");
+        address = in.nextLine();
+    }
+
+    public void output() {
+        System.out.println("Code: " + code);
+        System.out.println("Name: " + name);
+        System.out.println("Year: " + bYear);
+        System.out.println("Address: " + address);
+    }
+}
