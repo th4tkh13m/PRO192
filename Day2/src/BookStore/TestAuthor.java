db502034c77a7fa35c17fee910cef6721f073fca
th4tkh13m <khiemtonthat@gmail.com>
2021-09-16 15:36:39 +0700

    Add Book

---
+++ Day2/src/BookStore/TestAuthor.java
@@ -0,0 +1,8 @@
+package BookStore;
+
+public class TestAuthor {
+   public static void main(String[] args) {
+       Author khiem = new Author("Khiem", "khiem@gmail.com", 'm');
+       System.out.println("" + khiem);
+   } 
+}
