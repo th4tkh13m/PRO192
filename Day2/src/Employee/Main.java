58c1361c66712d8665c718712bd4b3ef285c5612
th4tkh13m <khiemtonthat@gmail.com>
2021-09-16 13:44:28 +0700

    Add employee main

--- Day2/src/Employee/Main.java
+++ Day2/src/Employee/Main.java
@@ -1,5 +1,16 @@
 package Employee;
 
+import java.text.NumberFormat;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Employee musk = new Employee(123, "Elon", "Musk", 1000000);
+        System.out.println("" + musk);
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        String salaryDisplay = formatter.format(musk.getAnnualSalary());
+        System.out.println("Annual Salary: " + salaryDisplay);
+        String raiseDisplay = formatter.format(musk.raiseSalary(5));
+        System.out.println("Raising salary: " + raiseDisplay);
+    }
 }
