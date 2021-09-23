651eab7072e4ffa0f10f413ad823b85667496162
th4tkh13m <khiemtonthat@gmail.com>
2021-09-23 14:24:00 +0700

    del

--- Day3/FBT/CourseDemo.java
+++
@@ -1,5 +0,0 @@
-package FBT;
-
-public class CourseDemo {
-    
-}
--- Day3/FBT/StudentDemo.java
+++
@@ -1,8 +0,0 @@
-package FBT;
-
-import java.util.Scanner;
-
-public class StudentDemo {
-    
-    public static Scanner in = new Scanner(System.in)
-}
