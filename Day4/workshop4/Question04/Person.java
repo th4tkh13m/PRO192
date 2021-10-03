ff80a699f1678446c2e5f89dd1159a2153f246f9
th4tkh13m <khiemtonthat@gmail.com>
2021-10-03 15:55:42 +0700

    Add Q4

---
+++ Day4/workshop4/Question04/Person.java
@@ -0,0 +1,28 @@
+package workshop4.Question04;
+
+public class Person {
+    private String name, address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", address=" + address + "]";
+    }
+
+}
---
+++ Day4/workshop4/Question04/Teacher.java
@@ -0,0 +1,50 @@
+package workshop4.Question04;
+
+public class Teacher extends Person {
+    private int numCourses; 
+    private String[] courses; 
+    private static final int MAX_COURSES = 5; 
+
+    public Teacher(String name, String address) {
+        super(name, address);
+        numCourses = 0;
+        courses = new String[MAX_COURSES];
+    }
+
+    public boolean addCourse(String course) {
+        for (int i = 0; i < numCourses; i++) {
+            if (courses[i].equals(course))
+                return false;
+        }
+        courses[numCourses] = course;
+        numCourses++;
+        return true;
+    }
+
+    public boolean removeCourse(String course) {
+        boolean found = false;
+        int courseIndex = -1;
+        for (int i = 0; i < numCourses; i++) {
+            if (courses[i].equals(course)) {
+                courseIndex = i;
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            for (int i = courseIndex; i < numCourses - 1; i++) {
+                courses[i] = courses[i + 1];
+            }
+            numCourses--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher: " + super.toString();
+    }
+    
+}
