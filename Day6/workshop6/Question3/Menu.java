7fe21159770f709a79bebdafc731dc1158ea3ca6
th4tkh13m <khiemtonthat@gmail.com>
2021-10-15 10:18:08 +0700

    Addq3

---
+++ Day6/workshop6/Question3/Menu.java
@@ -0,0 +1,22 @@
+package workshop6.Question3;
+
+import java.util.Scanner;
+
+/**
+ * Menu
+ */
+public class Menu {
+
+	Scanner sc = new Scanner(System.in);
+	public int menu() {
+		System.out.println("Menu");
+		System.out.println("1. Add");
+		System.out.println("2. Display");
+		System.out.println("3. Update");
+		System.out.println("4. Delete");
+		System.out.println("5. Exit");
+		System.out.println("Please enter your choice");
+		int choice = Validator.inputInt();
+		return choice;
+	}
+}
