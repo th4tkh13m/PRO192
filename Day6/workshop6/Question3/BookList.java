ced87f96a3410e8d6ec1ccc317ee90265b345897
th4tkh13m <khiemtonthat@gmail.com>
2021-10-16 08:11:11 +0700

    Edit book

--- Day6/workshop6/Question3/BookList.java
+++ Day6/workshop6/Question3/BookList.java
@@ -16,10 +16,32 @@ public class BookList {
 
 	public void addBook() {
 		while (true) {
-			Book book = new Book().
-			
+			Book book = new Book().createBook();
+			if (Validator.checkBookID(books, book.getId()) == -1) {
+				books.add(book);
+			} else {
+				System.out.println("The ID is existed!");
+			}
+			System.out.println("Continue?");
+			if (!Validator.inputYN()) {
+				break;
+			}
+		}
+	}
+	public void display() {
+		if (books.isEmpty()) {
+			System.out.println("No book.");
+			return;
+		}
+		for (Book book : books) {
+			System.out.println(book);
+		}
+	}
+	public void update() {
+		if (books.isEmpty()) {
+			System.out.println("No book.");
+			return;
 		}
 		
 	}
-
 }
