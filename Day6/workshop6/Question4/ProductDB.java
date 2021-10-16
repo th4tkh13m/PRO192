2eec6c09243ccfcefb6ea60fddddf0d6f55ce12b
th4tkh13m <khiemtonthat@gmail.com>
2021-10-16 21:17:28 +0700

    Add Q4

---
+++ Day6/workshop6/Question4/ProductDB.java
@@ -0,0 +1,21 @@
+package workshop6.Question4;
+
+public class ProductDB {
+	public static Product getProduct(String productCode) {
+		Product product = new Product();
+		product.setCode(productCode);
+		if (productCode.equalsIgnoreCase("java")) {
+			product.setDescription("Murach's Beginning Java");
+			product.setPrice(49.50);
+		} else if (productCode.equalsIgnoreCase("jsps")) {
+			product.setDescription("Murach's Java Servlets and JSP");
+			product.setPrice(49.50);
+		} else if (productCode.equalsIgnoreCase("mcb2")) {
+			product.setDescription("Murach's Mainframe COBOL");
+			product.setPrice(59.50);
+		} else {
+			product.setDescription("Unknown");
+		}
+		return product;
+	}
+}
