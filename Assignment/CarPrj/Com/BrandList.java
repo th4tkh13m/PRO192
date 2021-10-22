8bde8327f340a23b01e68d156628b04c56af60a4
th4tkh13m <khiemtonthat@gmail.com>
2021-10-22 15:35:36 +0000

    Add BrandList

---
+++ Assignment/CarPrj/Com/BrandList.java
@@ -0,0 +1,83 @@
+package Com;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * BrandList
+ */
+public class BrandList extends ArrayList<Brand> {
+
+	public BrandList() {
+	}
+
+	public boolean loadFromFile(String fname) {
+		// Clear the array if it is not null
+		if (this.size() > 0) {
+			this.clear();
+		}
+
+		// Try to read file
+		try {
+			File file = new File(fname);
+			if (!file.exists()) {
+				System.out.println("File does not exist");
+				return false;
+			}
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+
+			// Extract data
+			while ((line = bufferedReader.readLine()) != null) {
+				StringTokenizer tokenizer = new StringTokenizer(line, ",");
+				String brandId = tokenizer.nextToken();
+				String brandName = tokenizer.nextToken();
+				String soundAndPrice = tokenizer.nextToken();
+				tokenizer = new StringTokenizer(soundAndPrice, ":");
+				String soundBrand = tokenizer.nextToken();
+				int price = Integer.parseInt(tokenizer.nextToken());
+
+				Brand brand = new Brand(brandId, brandName, soundBrand, price);
+				this.add(brand);
+			}
+
+			// Close File
+			bufferedReader.close();
+			fileReader.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return true;
+	}
+
+	public boolean saveToFile(String fname) {
+		if (this.size() == 0) {
+			System.out.println("Empty Brand");
+			return false;
+		}
+		try {
+			File file = new File(fname);
+			FileWriter fileWriter = new FileWriter(file);
+			PrintWriter printWriter = new PrintWriter(fileWriter);
+
+			for (Brand brand : this) {
+				printWriter.println(brand.getBrandId() + "," + brand.getBrandName() + "," + brand.getSoundBrand() + "," + brand.getPrice());
+			}
+
+			printWriter.close();
+			fileWriter.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return true;
+	}
+
+}
---
+++ Assignment/CarPrj/Com/Car.java
@@ -0,0 +1,78 @@
+package Com;
+
+/**
+ * Car
+ */
+public class Car implements Comparable<Car> {
+
+	private String carID;
+	private Brand brand;
+	private String color, frameID, engineID;
+
+	public Car() {
+	}
+
+	public Car(String carID, Brand brand, String color, String frameID, String engineID) {
+		this.carID = carID;
+		this.brand = brand;
+		this.color = color;
+		this.frameID = frameID;
+		this.engineID = engineID;
+	}
+
+	public String getCarId() {
+		return carID;
+	}
+
+	public void setCarId(String carID) {
+		this.carID = carID;
+	}
+
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getFrameID() {
+		return frameID;
+	}
+
+	public void setFrameID(String frameID) {
+		this.frameID = frameID;
+	}
+
+	public String getEngineID() {
+		return engineID;
+	}
+
+	public void setEngineID(String engineID) {
+		this.engineID = engineID;
+	}
+
+	@Override
+	public String toString() {
+		return "Car{brand=" + brand + ", carID=" + carID + ", color=" + color + ", engineID=" + engineID + ", frameID="
+				+ frameID + "}";
+	}
+
+	public int compareTo(Car c) {
+		int d = this.brand.getBrandName().compareTo(c.brand.getBrandName());
+		if (d != 0) {
+			return d;
+		}
+		return this.carID.compareTo(c.carID);
+
+	}
+
+}
